5ca2c188e0d431928094261259a7f6053e6e24bf
nandigamnaresh <nandigamnaresh999@gmail.com>
2024-04-13 20:26:40 +0530

    gym2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import App2 from './App2';
+
+
+function App() {
+  return (
+    <div>
+      <App2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+import Naresh from 'axios'
+
+function App2() {
+
+    const [a, b]=useState();
+    const [c, d] =useState("shoulders")
+    const [e, f] =useState(20)
+
+
+
+
+    const Data = async () => {
+        const options = {
+            method: 'GET',
+            url: `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${c}`,
+            params: { limit: `${e}` },
+            headers: {
+                // 'X-RapidAPI-Key': '5d2f083533mshcbe5342d0ed436cp1754a1jsn0ecf5e5e1777',
+                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+            }
+        };
+
+
+        try {
+            const response = await Naresh.request(options);
+            console.log(response.data);
+            b(response.data)
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    useEffect(() => {
+        Data()
+    }, [c,e])
+
+    const chinna =(e)=>{
+        d(e.target.value)
+    }
+
+    const chinna1 =(e)=>{
+        f(e.target.value)
+    }
+
+    // arms, chest, back, shoulders, core, and legs.
+
+
+
+
+    return (
+        <div>
+
+            <h2 className='text-center  fw-bold pb-5 pt-3' style={{color:"GREEN" ,border:"2px solid white",backgroundColor:"white"}}>GYM PRACTICE2</h2>
+            <div className='container'> 
+                <div className='row ms-5'>
+                    <input className='fw-900 ms-5 mt-3 mb-2' type='text' placeholder='SEARCH BODY PART NAME' onChange={chinna} />
+                    <input className='fw-900 ms-5 mt-3 mb-2' type='number' placeholder='LIMIT' onChange={chinna1} value={e} />
+
+                    
+                    {
+                       a&& a.map(data => 
+                            <div className='col-lg-4 pb-5 ps-5'>
+                                
+                                    <div class="card mt-4" style={{ width: "18rem" }} id='main-body'>
+                                        <div class="card-body  pt-4 pb-4 ">
+                                            <h3 class="card-title ps-3">BODYPART: {data.bodyPart.toUpperCase()}</h3>
+                                            <h6 className='card-text ms-3'>TARGET : {data.target.toUpperCase()}</h6>
+                                            <img src={data.gifUrl} controls style={{width:"100%"}}/>
+                                            <h5 className='mt-3 card-text'>INSTRUCTIONS Name : {data.name}</h5>
+                                            <h5 className='mt-3 mb-3 card-text'>INSTRUCTIONS</h5>
+                                            <p>1.{data.instructions[0]}</p>
+                                            <p>2.{data.instructions[1]}</p>
+                                            <p>3.{data.instructions[2]}</p>
+                                            <p>4.{data.instructions[3]}</p>
+                                            <p>5.{data.instructions[4]}</p>
+
+                                            <h6 className='card-text mt-4'>EQUIPMENT : {data.equipment}</h6>
+                                            <p>SECONDARY MUSCLES :{data.secondaryMuscles[0]} {data.secondaryMuscles[1]}</p>
+
+                                        </div>
+                                    </div>
+
+                            </div>
+                        )
+                    }
+
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    )
+}
+
+export default App2
